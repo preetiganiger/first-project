0b4f223ec6ff5ce3c2e044aede77ba3ee9082479
preetiganiger <preetiganiger1111@gmail.com>
2024-08-02 12:55:20 +0530

    Add files via upload

---
+++ preeti.py
@@ -0,0 +1,20 @@
+
+
+# x = input("enter the number x:")
+# print(8)
+# y=input("enter the number y:")
+# z=input("enter the numeber z:")
+# if x>=y and x>=z:
+# 	print("x is greater")
+# elif y>=z:
+# 	print("y is greater")
+# else:
+#     print
+
+n=input("enter the ")
+num=0
+for i in range (1,n)
+
+    elem=input("enter number ")
+num+=elem
+print(num)
